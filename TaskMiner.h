68c1ca406f524ae32d081b5eff917df54ef02ce1
Pedro Ramos <root@Pedros-MacBook-Pro-2.local>
2017-01-20 10:45:33 -0200

    fixed bug in taskminer module

---
+++ TaskMiner.h
@@ -0,0 +1,109 @@
+//===------------------------------- TaskMiner.h --------------------------===//
+//
+// Author: Pedro Ramos (pedroramos@dcc.ufmg.br)
+//
+//===----------------------------------------------------------------------===//
+//
+//                           The LLVM Compiler Infrastructure
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef TASKMINER_H
+#define TASKMINER_H
+
+//LLVM IMPORTS
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/ADT/SmallSet.h"
+
+//LOCAL IMPORTS
+#include "DepAnalysis.h"
+
+//STL IMPORTS
+#include <list>
+#include <set>
+#include <iostream>
+#include <string>
+
+namespace llvm
+{
+	class Task;
+	class FunctionCallTask;
+	// class NestedLoopTask;
+	// class CodeFragmentTask;
+
+	enum AccessType {UNKNOWN=0, READ=1, WRITE=2, READWRITE=3};
+
+	inline AccessType operator|(AccessType a, AccessType b)
+	{
+		return static_cast<AccessType>(static_cast<int>(a) | static_cast<int>(b)); 
+	}
+
+	class TaskMiner : public FunctionPass
+	{
+	public:
+
+		static char ID;
+		TaskMiner() : FunctionPass(ID) {}
+		~TaskMiner() {};
+		void getAnalysisUsage(AnalysisUsage &AU) const override;
+		bool runOnFunction(Function &func) override;
+		std::list<Task*>* getTasks();
+		Task* getTaskFromParentLoop(Loop* L);
+
+		struct LoopData
+		{
+			Instruction* indVar;
+			std::list<BasicBlock*> innerBBs;
+
+			//Debugginf purposes only
+			void print();
+		};
+
+	private:
+		std::list<Task*> tasks;
+		void resolveInsAndOutsSets();
+	};
+
+	class Task
+	{
+	public:
+		Task(Loop* parent) : parent(parent) {};
+		virtual ~Task() {};
+		Loop* getParent();
+		virtual bool resolveInsAndOutsSets() { return false; };
+		std::set<Value*> getLiveIN() const;
+		std::set<Value*> getLiveOUT() const;
+		std::set<Value*> getLiveINOUT() const;
+
+		//Only for debugging purposes. STDOUT/errs()
+		virtual void print();
+		void printLiveSets();
+
+	protected:
+		Loop* parent;
+		std::set<Value*> liveIN;
+		std::set<Value*> liveOUT;
+		std::set<Value*> liveINOUT;
+		AccessType getTypeFromInst(Instruction* I);
+		std::string accessTypeToStr(AccessType T);	
+	};
+
+	class FunctionCallTask : public Task
+	{
+	public:
+		FunctionCallTask(Loop* parent, CallInst* CI) : Task(parent), functionCall(CI) {};
+		~FunctionCallTask() {};
+		CallInst* getFunctionCall();
+		bool resolveInsAndOutsSets() override;
+		void print() override;
+		
+	private:
+		CallInst* functionCall;
+	};
+
+}
+
+#endif
