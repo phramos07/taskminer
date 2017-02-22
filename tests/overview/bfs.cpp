9ce5c68e32787d9390e2dddcec6157f48632be4d
Pedro Ramos <root@dhcp200-22.usuarios.dcc.ufmg.br>
2017-02-22 11:09:05 -0300

    Added Guido's modifications to the paper and the example of section 2

--- tests/overview/bfs.cpp
+++ tests/overview/bfs.cpp
@@ -1,27 +1,63 @@
 #include <iostream>
 #include <omp.h>
-#define N 100
+#include <math.h>
+#define N 5
 
-void bfs(int* G, int* node, int* neigh);
+void bfs(int** G, int* node, int* neigh);
+
+void fillgraph(int** G);
+
+void printGraph(int** G);
 
 int main()
 {
-	int* G = (int*)malloc(N*N*sizeof(int));
+	int **G;
+	G = (int**)malloc(N*sizeof(int*));
+	for (int i=0; i<N; i++)
+		G[i] = (int*)malloc(N*sizeof(int));
 	int neigh[N];
+	int visited[N];
 
-	bfs(G, &G[0], neigh);
+	fillgraph(G);
+	printGraph(G);
+
+	// bfs(G, G[0], neigh);
+
+	for (int i = 0; i < N; i++)
+		std::cout << "Node " << i << " has " << neigh[i] << " neighbors" << std::endl;
 
 	return 0;
 }
 
-void bfs(int* G, int* node, int* neigh)
+void bfs(int** G, int* node,int* neigh)
 {
 	for (int i=0; i<N; i++)
 		if (node[i])
 		{
 			neigh[i]++;
-			bfs(G, &G[i*N], neigh);			
+			bfs(G, G[i], neigh);			
 		}
+}
+
+void fillgraph(int** G)
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			G[i][j] = rand()%N;
+		}
+}
+
+void printGraph(int** G)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			std::cout << G[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
 
 
