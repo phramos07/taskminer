480a0bb30926619775313b1ebcd7586f1c277b50
Pedro Ramos <root@dhcp200-22.usuarios.dcc.ufmg.br>
2017-02-22 13:11:03 -0300

    Fixed a bug in TaskMiner + Changed the example of section 2

--- tests/overview/bfs.cpp
+++ tests/overview/bfs.cpp
@@ -3,25 +3,21 @@
 #include <math.h>
 #define N 5
 
-void bfs(int** G, int* node, int* neigh);
+void bfs(int* G, int* node, int* neigh);
 
-void fillgraph(int** G);
+void fillgraph(int* G);
 
-void printGraph(int** G);
+void printGraph(int* G);
 
 int main()
 {
-	int **G;
-	G = (int**)malloc(N*sizeof(int*));
-	for (int i=0; i<N; i++)
-		G[i] = (int*)malloc(N*sizeof(int));
+	int* G = (int*)malloc(N*N*sizeof(int));
 	int neigh[N];
-	int visited[N];
 
 	fillgraph(G);
 	printGraph(G);
 
-	// bfs(G, G[0], neigh);
+	bfs(G, &G[0], neigh);
 
 	for (int i = 0; i < N; i++)
 		std::cout << "Node " << i << " has " << neigh[i] << " neighbors" << std::endl;
@@ -29,32 +25,32 @@ int main()
 	return 0;
 }
 
-void bfs(int** G, int* node,int* neigh)
+void bfs(int* G, int* node,int* neigh)
 {
 	for (int i=0; i<N; i++)
-		if (node[i])
+		if (*(node + i))
 		{
 			neigh[i]++;
-			bfs(G, G[i], neigh);			
+			bfs(G, &G[i*N], neigh);			
 		}
 }
 
-void fillgraph(int** G)
+void fillgraph(int* G)
 {
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
 		{
-			G[i][j] = rand()%N;
+			G[i*N + j] = rand()%N;
 		}
 }
 
-void printGraph(int** G)
+void printGraph(int* G)
 {
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			std::cout << G[i][j] << " ";
+			std::cout << G[i*N + j] << " ";
 		}
 		std::cout << std::endl;
 	}
