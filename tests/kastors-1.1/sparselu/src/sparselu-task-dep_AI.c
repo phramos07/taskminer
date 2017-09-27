fddb67907667d64ac9ab4fd84fa9ebfada17b265
gleison <gleison14051994@gmail.com>
2017-09-27 17:46:38 -0300

    TaskMiner - sparselu 2.0 annotated.

--- tests/kastors-1.1/sparselu/src/sparselu-task-dep_AI.c
+++ tests/kastors-1.1/sparselu/src/sparselu-task-dep_AI.c
@@ -36,28 +36,53 @@ void sparselu_par_call(float **BENCH, int matrix_size, int submatrix_size) {
       long long int TM5[2];
       TM5[0] = kk * matrix_size;
       TM5[1] = TM5[0] + kk;
-      #pragma omp task depend(in:submatrix_size) depend(inout:BENCH[TM5[1]])
+      #pragma omp task depend(inout:BENCH[TM5[1]])
       lu0(BENCH[kk * matrix_size + kk], submatrix_size);
+      #pragma omp parallel
+      #pragma omp single
       for (jj = kk + 1; jj < matrix_size; jj++) {
         if (BENCH[kk * matrix_size + jj] != NULL) {
+          long long int TM7[3];
+          TM7[0] = kk * matrix_size;
+          TM7[1] = TM7[0] + kk;
+          TM7[2] = TM7[0] + jj;
+          #pragma omp task depend(in:BENCH[TM7[1]]) depend(inout:BENCH[TM7[2]])
           fwd(BENCH[kk * matrix_size + kk], BENCH[kk * matrix_size + jj],
               submatrix_size);
         }
       }
+      #pragma omp parallel
+      #pragma omp single
       for (ii = kk + 1; ii < matrix_size; ii++) {
         if (BENCH[ii * matrix_size + kk] != NULL) {
+          long long int TM10[4];
+          TM10[0] = kk * matrix_size;
+          TM10[1] = TM10[0] + kk;
+          TM10[2] = ii * matrix_size;
+          TM10[3] = TM10[2] + kk;
+          #pragma omp task depend(in:BENCH[TM10[1]]) depend(inout:BENCH[TM10[3]])
           bdiv(BENCH[kk * matrix_size + kk], BENCH[ii * matrix_size + kk],
                submatrix_size);
         }
       }
       for (ii = kk + 1; ii < matrix_size; ii++) {
         if (BENCH[ii * matrix_size + kk] != NULL) {
+          #pragma omp parallel
+          #pragma omp single
           for (jj = kk + 1; jj < matrix_size; jj++) {
             if (BENCH[kk * matrix_size + jj] != NULL) {
               if (BENCH[ii * matrix_size + jj] == NULL) {
                 BENCH[ii * matrix_size + jj] =
+                    #pragma omp task
                     allocate_clean_block(submatrix_size);
               }
+              long long int TM15[5];
+              TM15[0] = ii * matrix_size;
+              TM15[1] = TM15[0] + kk;
+              TM15[2] = kk * matrix_size;
+              TM15[3] = TM15[2] + jj;
+              TM15[4] = TM15[0] + jj;
+              #pragma omp task depend(in:BENCH[TM15[1]],BENCH[TM15[3]]) depend(inout:BENCH[TM15[4]])
               bmod(BENCH[ii * matrix_size + kk], BENCH[kk * matrix_size + jj],
                    BENCH[ii * matrix_size + jj], submatrix_size);
             }
