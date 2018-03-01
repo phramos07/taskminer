89a349bf7831e80b6821494bf515bcb0370a3fef
gleison <gleison14051994@gmail.com>
2018-03-01 08:10:56 -0300

    Private benchmarck available.

--- tests/benchmarks/toys/private/priv_AI.c
+++ tests/benchmarks/toys/private/priv_AI.c
@@ -14,6 +14,11 @@ int sum_range(int *V, int N, int L, int U, int *A) {
   #pragma omp single
   for (i = 0; i < N; i++) {
     {
+    long long int TM14[2];
+    TM14[0] = L > 0;
+    TM14[1] = (TM14[0] ? L : 0);
+    int tmc2 = TM14[1] * (15);
+    int tm_cost1 = (18 + tmc2);
     long long int TM13[14];
     TM13[0] = L > 0;
     TM13[1] = (TM13[0] ? L : 0);
@@ -29,8 +34,6 @@ int sum_range(int *V, int N, int L, int U, int *A) {
     TM13[11] = TM13[10] / 4;
     TM13[12] = (TM13[11] > 0);
     TM13[13] = (TM13[12] ? TM13[11] : 0);
-    int tmc2 = TM14[1] * (15);
-    int tm_cost1 = (18 + tmc2);
     #pragma omp task depend(inout: A[0:TM13[13]],V[0:TM13[6]]) if(tm_cost1 > 6000)
     {
     int j = 0; // V[i];
